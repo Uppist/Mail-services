bf6925fe1b24e8e3f140acb7017b5f06b8f83533
KAMAH <limitlesskds@gmail.com>
2023-03-20 02:51:30 +0100

    Setup contact for for swift website

---
+++ mail.js
@@ -0,0 +1,52 @@
+import express, { json } from 'express';
+import { createTransport } from "nodemailer";
+
+const app = express();
+
+app.use(express.json())
+
+async function main() {
+
+    let transporter = createTransport({
+        service: "gmail",
+        port: 587,
+        auth: {
+            user: process.env.USER,
+            pass: process.env.PASS
+        }
+    })
+
+    app.post('/contact', async function(req,res) {
+        if (req.body.from === '' || req.body.subject === ''|| req.body.message === ''){
+            return res.status(400).json({error: "Field cannot be empty"})
+        }
+        let info = await transporter.sendMail({
+            from: req.body.from,
+            to: "uppist.tech@gmail.com",
+            subject: req.body.subject,
+            html: `<h3> ${req.body.message} </h3>`
+        })
+        console.log(info.messageId)
+
+        return res.status(200).json({"message": info.response})
+    })
+
+    app.post('/reservation', async function(req, res) {
+        let info = await transporter.sendMail({
+            from: req.body.from,
+            to: "uppist.tech@gmail.com",
+            subject: " [Table Reservations] ",
+            html: `<h3> ${req.body.date} \n ${req.body.time} \n
+                ${req.body.name}\n ${req.body.email} \n ${req.body.phone} \n
+                ${req.body.NOG}\n ${req.body.message}</h3>`
+        })
+    })
+}
+
+main().catch((err) => console.log(`An error occured ${err}`))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, function server(){
+    console.log(`Server listening for connections on port ${PORT}`)
+})
